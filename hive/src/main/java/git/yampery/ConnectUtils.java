3348717e84e434779b732ba14f5b9027d209ef7e
yampery <yampery@163.com>
2018-09-26 17:10:44 +0800

    提交==> 大数据模块，初始提交

---
+++ hive/src/main/java/git/yampery/ConnectUtils.java
@@ -0,0 +1,13 @@
+package git.yampery;
+
+/**
+ * @decription ConnectUtils
+ * <p>hive连接</p>
+ * @author Yampery
+ * @date 2018/9/26 16:46
+ */
+public class ConnectUtils {
+
+    private static final String DRIVER_NAME = "org.apache.hive.jdbc.HiveDriver";
+    private static final String URL = "jdbc:hive2://node2.xdata:10000/default";
+}
